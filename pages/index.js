86fccd1b2bc63f67ee8effacbfd824cfa2e5b90e
Nick Jenson <nickjenson@gmail.com>
2020-08-02 12:06:57 -0600

    fix apple-touch-icon

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Home({ allPostsData }) {
     <Layout home>
       <Head>
         <title>{name}</title>
-        <link rel="apple-touch-icon" href="apple-touch-icon.png"></link>
       </Head>
       <section className={utils.headingMd}>
         <ul className={utils.list}>
